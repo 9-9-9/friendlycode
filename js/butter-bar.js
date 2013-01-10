d023543111485dc12db79f642bd518fb2ab4991d
Atul Varma <varmaa@gmail.com>
2013-01-10 10:32:02 -0500

    init butter before editor

--- js/butter-bar.js
+++ js/butter-bar.js
@@ -4,26 +4,33 @@ define([
   "fc/ui/mark-tracker",
   "fc/ui/preview-to-editor-mapping"
 ], function($, _, MarkTracker, PreviewToEditorMapping) {
-  return function ButterBar(editor) {
+  var getParallelNode = PreviewToEditorMapping._getParallelNode;
+  var nodeToCode = PreviewToEditorMapping._nodeToCode;
+
+  function ButterBar(butterIframe) {
+    var self = {};
+    var editor = null;
     var lastDocFrag = null;
     var lastPreviewWindow = null;
     var Instapoppin = null;
-    var Butter = null;
-    var getParallelNode = PreviewToEditorMapping._getParallelNode;
-    var nodeToCode = PreviewToEditorMapping._nodeToCode;
+    var marks = null;
+    var butterWindow = butterIframe.contentWindow;
+    var Butter = butterWindow.Butter;
+    var tray = butterWindow.document.querySelector(".butter-tray");
     var maybeRemoved = [];
     var checkIfReallyRemoved;
 
-    var onButterReady = function(butter, butterWindow, iframe) {
-      var tray = butterWindow.document.querySelector(".butter-tray");
-      iframe.style.height = tray.offsetHeight + "px";
-      var fcHolder = document.getElementById("bare-fc-holder");
-      fcHolder.setAttribute("style",
+    butterIframe.style.height = tray.offsetHeight + "px";
+    
+    self.bindToEditor = function(friendlycodeEditor) {
+      editor = friendlycodeEditor;
+      editor.editor.container.attr("style",
         "height: calc(100% - " + tray.offsetHeight + "px);" +
         "height: -webkit-calc(100% - " + tray.offsetHeight + "px);"
       );
       editor.codeMirror.refresh();
-      Butter = butter;
+      marks = MarkTracker(editor.codeMirror);
+
       $(tray).on("mouseenter", ".butter-track-event", function(e) {
         var target = e.target;
         if (!$(target).hasClass("butter-track-event"))
@@ -106,61 +113,61 @@ define([
             Instapoppin.pop.media.duration)
           Instapoppin.pop.media.currentTime = e.target.currentTime;
       });
-      if (lastPreviewWindow)
-        editor.codeMirror.reparse();
-    };
-  
-    var marks = MarkTracker(editor.codeMirror);
-    editor.editor.panes.preview.on("refresh", function(event) {
-      lastDocFrag = event.documentFragment;
-      lastPreviewWindow = event.window;
-      var documentElement = event.window.document.documentElement;
-      documentElement.addEventListener("instapoppinactive", function(e) {
-        if (!Butter) return;
-        Instapoppin = event.window.Instapoppin;
-        var media = Butter.app.currentMedia;
-        var previewMedia = Instapoppin.pop.media;
-        var syncDuration = function() {
-          Butter.app.currentMedia.url = "#t=," + previewMedia.duration;
-          setTimeout(function() {
-            // We shouldn't have to do this setTimeout, but media elements
-            // with controls seem to not cause popcorn to be notified if
-            // we do it without a timeout in Chrome.
-            previewMedia.currentTime = Butter.app.currentMedia.currentTime;
-          }, 100);
-        };
-        if (!previewMedia.duration)
-          previewMedia.addEventListener("loadedmetadata", syncDuration, 
-                                        false);
-        else
-          syncDuration();
-        media.tracks.slice().forEach(function(track) {
-          media.removeTrack(track);
-        });
-        Instapoppin.getParticipatingElements().forEach(function(elem) {
-          var durations = Instapoppin.getActiveDurations(elem);
-          durations.forEach(function(duration) {
-            if (!media.tracks.length)
-              Butter.app.currentMedia.addTrack();
-            var track = media.tracks[0];
-            var text = _.escape(elem.textContent) ||
-                       '&lt;' + elem.nodeName.toLowerCase() + '&gt;';
-            var trackEvent = track.addTrackEvent({
-              type: "html",
-              popcornOptions: {
-                _element: elem,
-                start: duration.start,
-                end: duration.end,
-                html: text
-              }
-            });
-            trackEvent.view.element._trackEvent = trackEvent;
+
+      editor.editor.panes.preview.on("refresh", function(event) {
+        lastDocFrag = event.documentFragment;
+        lastPreviewWindow = event.window;
+        var documentElement = event.window.document.documentElement;
+        documentElement.addEventListener("instapoppinactive", function(e) {
+          Instapoppin = event.window.Instapoppin;
+          var media = Butter.app.currentMedia;
+          var previewMedia = Instapoppin.pop.media;
+          var syncDuration = function() {
+            Butter.app.currentMedia.url = "#t=," + previewMedia.duration;
+            event.window.setTimeout(function() {
+              // We shouldn't have to do this setTimeout, but media elements
+              // with controls seem to not cause popcorn to be notified if
+              // we do it without a timeout in Chrome.
+              previewMedia.currentTime = Butter.app.currentMedia.currentTime;
+            }, 100);
+          };
+          if (!previewMedia.duration)
+            previewMedia.addEventListener("loadedmetadata", syncDuration, 
+                                          false);
+          else
+            syncDuration();
+          media.tracks.slice().forEach(function(track) {
+            media.removeTrack(track);
           });
-        });
-      }, true);
-    });
+          Instapoppin.getParticipatingElements().forEach(function(elem) {
+            var durations = Instapoppin.getActiveDurations(elem);
+            durations.forEach(function(duration) {
+              if (!media.tracks.length)
+                Butter.app.currentMedia.addTrack();
+              var track = media.tracks[0];
+              var text = _.escape(elem.textContent) ||
+                         '&lt;' + elem.nodeName.toLowerCase() + '&gt;';
+              var trackEvent = track.addTrackEvent({
+                type: "html",
+                popcornOptions: {
+                  _element: elem,
+                  start: duration.start,
+                  end: duration.end,
+                  html: text
+                }
+              });
+              trackEvent.view.element._trackEvent = trackEvent;
+            });
+          });
+        }, true);
+      });
+    };
+    
+    return self;
+  };
   
-    editor.ready.done(function() {
+  var exports = {
+    create: function(src, onReady) {
       var iframe = document.createElement("iframe");
       iframe.setAttribute("class", "butter");
       iframe.src = "butter/templates/basic/";
@@ -171,9 +178,11 @@ define([
         // it's a proxy for that.
         if (w.Butter && w.Butter.app && w.Butter.app.project) {
           clearInterval(interval);
-          onButterReady(w.Butter, w, iframe);
+          onReady(new ButterBar(iframe));
         }
       }, 50);
-    });
+    }
   };
+
+  return exports;
 });
